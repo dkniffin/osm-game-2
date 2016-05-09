7a8be56d7b83010c12fcd371c1bc40bc33a0f671
Derek Kniffin <derek.kniffin@gmail.com>
2016-05-09 10:38:23 -0400

    Initial commit
    Includes basic polygon rendering

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Building : MonoBehaviour {
+
+	public Vector2[] vertices;
+
+	// Use this for initialization
+	void Start () {
+		UpdateMesh (vertices);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void UpdateMesh(Vector2[] vertices) {
+		MeshFilter mf = GetComponent<MeshFilter> ();
+		Mesh mesh = new Mesh ();
+		mf.mesh = mesh;
+
+		Vector3[] mesh_vertices = new Vector3[vertices.Length];
+		Vector3[] normals = new Vector3[vertices.Length];
+		for (int i = 0; i < vertices.Length; i++) {
+			Vector2 vertex = vertices [i];
+			// Set up 3d vertex from 2d vertex
+			mesh_vertices [i] = new Vector3 (vertex.x, vertex.y, 0);
+
+			// The normal is always the same, since we're in 2d
+			normals [i] = Vector3.back;
+		}
+
+		// Use the triangulator to get indices for creating triangles
+		Triangulator tr = new Triangulator(vertices);
+		int[] indices = tr.Triangulate();
+
+		mesh.vertices = mesh_vertices;
+		mesh.triangles = indices;
+		mesh.normals = normals;
+	}
+}
---
+++ Assets/Scripts/BuildingImporter.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BuildingImporter : MonoBehaviour {
+
+	public GameObject prefab;
+	public List<Building> buildings;
+	public TextAsset osmFile;
+
+	public float width = 100f;
+	public float height = 50f;
+
+	private Dictionary<int, OSMNode> nodes;
+	private Dictionary<int, OSMWay> ways;
+
+	// Use this for initialization
+	void Start () {
+		Vector2[] vertices = new Vector2[] {
+			new Vector2(0, 0),
+			new Vector2(0, height),
+			new Vector2(width, height),
+			new Vector2(width, 0)
+		};
+		GameObject buildingObject = (GameObject)Instantiate (prefab, new Vector3(0, 0, 0), Quaternion.identity);
+		Building b = buildingObject.GetComponent<Building> ();
+		b.vertices = vertices;
+		buildings.Add (b);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
