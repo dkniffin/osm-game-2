2ae41dbd347e6971faa2b6c5301beb6a59bcc1dc
Derek Kniffin <derek.kniffin@gmail.com>
2016-05-21 20:29:51 -0400

    On click, remove notification, and update existing notification positions

--- Assets/Scripts/Notifications.cs
+++ Assets/Scripts/Notifications.cs
@@ -9,14 +9,14 @@ public class Notifications : MonoBehaviour {
 
 	private float currentYPosition = 0.0f;
 
-	void OnEnable()
-	{
+	private List<GameObject> notifications = new List<GameObject>();
+
+	void OnEnable()  {
 		EventManager.OnGameEvent += Add;
 	}
 
 
-	void OnDisable()
-	{
+	void OnDisable() {
 		EventManager.OnGameEvent -= Add;
 	}
 
@@ -24,24 +24,44 @@ public class Notifications : MonoBehaviour {
 		// Build a notification button, and set it's position
 		GameObject notification = Instantiate(notificationPrefab) as GameObject;
 		notification.transform.SetParent(this.transform, false);
-		var pos = notification.transform.localPosition;
-		notification.transform.localPosition = new Vector3(pos.x, currentYPosition, 0);
-		currentYPosition -= notification.GetComponent<RectTransform>().rect.height;
+		SetPosition (notification);
 
 		// Set the text for the notification
 		Text notificationText = notification.GetComponentInChildren<Text>();
 		notificationText.text = eventText;
 
+		// Add it to the queue
+		notifications.Add(notification);
+
 		// Add an onClick listener for it
 		Button b = notification.GetComponent<Button>();
 		b.onClick.AddListener (delegate {
 			MoveCameraTo (eventLocation);
-//			notification.SetActive(false);
+			RemoveNotification(notification);
 		});
 	}
 
 	private void MoveCameraTo(Vector2 location) {
 		var newCameraPosition = new Vector3(location.x, location.y, Camera.main.transform.position.z);
 		Camera.main.transform.position = newCameraPosition;
+	}
+
+	private void RemoveNotification(GameObject notification) {
+		notification.SetActive(false);
+		notifications.Remove (notification);
+		UpdatePositions ();
+	}
+
+	private void UpdatePositions() {
+		currentYPosition = 0.0f;
+		foreach (GameObject n in notifications) {
+			SetPosition (n);
+		}
+	}
+
+	private void SetPosition(GameObject notification) {
+		var pos = notification.transform.localPosition;
+		notification.transform.localPosition = new Vector3(pos.x, currentYPosition, 0);
+		currentYPosition -= notification.GetComponent<RectTransform>().rect.height;
 	}
 }
